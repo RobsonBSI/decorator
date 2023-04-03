3d1878cecac5a3d9d810449b01d267617b973bb5
RobsonBSI <103039603+RobsonBSI@users.noreply.github.com>
2023-04-03 10:49:56 -0300

    atu

--- src/main/java/BarDrinks.java
+++ src/main/java/BarDrinks.java
@@ -1,2 +1,15 @@
-public class BarDrinks {
+public class BarDrinks extends BuffetDecorator{
+    public BarDrinks(Buffet buffet) {
+        super(buffet);
+    }
+
+    @Override
+    public float getCustoPessoa() {
+        return 100.0f;
+    }
+
+    @Override
+    public String getProdutoEvento() {
+        return "Bar de Drinks";
+    }
 }
